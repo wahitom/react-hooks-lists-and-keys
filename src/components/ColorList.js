c2fac6bd44c564bfad4ed853732cd8648c51887b
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-10-27 00:25:00 +0300

    Completed

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -7,20 +7,40 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "blue",
   ];
+
+  const colorElements = colors.map((color) => {
+    //adding a key to each element so that react can keep track of it
+    //we are going to make the name of each color its key because keys have to be unique
+    //key props don't show up with the rest of the props in our function because it is meant to be used internally by React
+
+    return (
+      <li key={color} style={{ color: color }}>
+        {color}
+      </li>
+    );
+    //here we added a style attribute that allows the text color to change according to what color is written
+  });
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
+      <ol>{colorElements}</ol>
     </div>
   );
 }
+
+//Assigning key with objects its often best to use the id as the key
+/**
+ * const users = [
+  { id: 1, firstName: "Duane", lastName: "Watson" },
+  { id: 2, firstName: "Duane", lastName: "Johnson" },
+];
+
+const userHeadings = users.map((user) => {
+  return <h1 key={user.id}>{user.firstName}</h1>;
+});
+ */
 
 export default ColorList;
